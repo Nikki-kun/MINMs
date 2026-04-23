2950ee332c550588c0ceed0b58adc8185d32b75b
Nikki-kun <arikovicaroslav4@gmail.com>
2026-04-23 23:34:51 +0300

    add object name

--- MINMs.Server/Database/Migrations/002_add_avatar.sql
+++ MINMs.Server/Database/Migrations/002_add_avatar.sql
@@ -2,6 +2,10 @@ ALTER TABLE `users`
 ADD COLUMN `avatar_id` INT NULL
 AFTER `password_hash`;
 
+ALTER TABLE `files` 
+ADD COLUMN `object_name` VARCHAR(500) NULL
+AFTER `original_filename`;
+
 ALTER TABLE `users`
 ADD CONSTRAINT `fk_users_avatar`
 FOREIGN KEY (`avatar_id`) REFERENCES `files`(`file_id`)
